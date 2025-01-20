fb4a4433a41c40256ad4decffd56d15cb30693fd
chirag-purohit <chirag-purohit>
2025-01-20 09:52:24 +0000

    customized dates

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,39 +1,132 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 import { Agenda, DateData, AgendaEntry, AgendaSchedule } from 'react-native-calendars';
 import testIDs from '../testIDs';
 
+const staticData = {
+  "auctions": [
+    {
+      "id": "3200",
+      "name": " The Inner Circle, A Napa Local's Collection, New York, October 7 - October 20",
+      "type": "Live",
+      "status": "STARTED",
+      "lotCount": 1,
+      "startDate": "2024-11-31T17:24:04+00:00",
+      "endDate": null,
+      "url": "http://sam.local/auctions/catalog/id/3200"
+    },
+    {
+      "id": "3171",
+      "name": " REPORTING TEST LIVE SALE",
+      "type": "Live",
+      "status": "STARTED",
+      "lotCount": 9,
+      "startDate": "2024-11-18T20:13:58+00:00",
+      "endDate": null,
+      "url": "http://sam.local/auctions/catalog/id/3171"
+    },
+    {
+      "id": "3165",
+      "name": "Fine & Rare Wines & Spirits, New York, June 28",
+      "type": "Live",
+      "status": "STARTED",
+      "lotCount": 9,
+      "startDate": "2024-12-17T07:24:27+00:00",
+      "endDate": null,
+      "url": "http://sam.local/auctions/catalog/id/3165"
+    },
+    {
+      "id": "3152",
+      "name": "REPORTING TEST TIMED SALE",
+      "type": "Timed",
+      "status": "STARTED",
+      "lotCount": 10,
+      "startDate": "2024-11-13T06:38:19+00:00",
+      "endDate": null,
+      "url": "http://sam.local/auctions/catalog/id/3152"
+    },
+    {
+      "id": "3143",
+      "name": "Hong Kong Live Admin Test Sale - September 2024",
+      "type": "Live",
+      "status": "STARTED",
+      "lotCount": 10,
+      "startDate": "2024-11-03T09:31:10+00:00",
+      "endDate": "2024-11-06T09:31:10+00:00",
+      "url": "http://sam.local/auctions/catalog/id/3143"
+    },
+    {
+      "id": "3093",
+      "name": "PSA UAT - A Winemaker's Cellar and Fine & Rare Wines & Spirits",
+      "type": "Timed",
+      "status": "STARTED",
+      "lotCount": 4,
+      "startDate": "2024-11-27T22:12:48+00:00",
+      "endDate": null,
+      "url": "http://sam.local/auctions/catalog/id/3093"
+    },
+    {
+      "id": "3055",
+      "name": " PSA UAT - Fine & Rare Wines & Spirits, Hong Kong, December 1 & 2",
+      "type": "Live",
+      "status": "STARTED",
+      "lotCount": 0,
+      "startDate": "2024-12-01T10:01:56+00:00",
+      "url": "http://sam.local/auctions/catalog/id/3055"
+    },
+    {
+      "id": "2975",
+      "name": " FREYES TEST PSA UAT - Fine & Rare Wines & Spirits, Hong Kong, December 1 & 2",
+      "type": "Hybrid",
+      "status": "STARTED",
+      "lotCount": 0,
+      "startDate": "2024-12-01T10:01:56+00:00",
+      "endDate": null,
+      "url": "http://sam.local/auctions/catalog/id/2975"
+    },
+    {
+      "id": "2964",
+      "name": " post-auction import test",
+      "type": "Hybrid",
+      "status": "STARTED",
+      "lotCount": 10,
+      "startDate": "2024-11-26T23:15:11+00:00",
+      "endDate": null,
+      "url": "http://sam.local/auctions/catalog/id/2964"
+    },
+    {
+      "id": "5883",
+      "name": "Fine & Rare Wines & Spirits, Hong Kong, May 14",
+      "type": "Hybrid",
+      "status": "STARTED",
+      "lotCount": 21,
+      "startDate": "2024-11-22T06:03:49+00:00",
+      "endDate": null,
+      "url": "http://sam.local/auctions/catalog/id/5883"
+    }
+  ]
+};
+
 export default function TabTwoScreen() {
   const [items, setItems] = useState<AgendaSchedule>({});
 
-  const loadItems = (day: DateData) => {
-    const newItems = { ...items };
+  useEffect(() => {
+    const newItems: AgendaSchedule = {};
 
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
-
-        // Randomly decide whether to add records for this day
-        if (Math.random() > 0.3) { // 70% chance to add records
-          if (!newItems[strTime]) {
-            newItems[strTime] = [];
-
-            const numItems = Math.floor(Math.random() * 3 + 1);
-            for (let j = 0; j < numItems; j++) {
-              newItems[strTime].push({
-                name: 'Item for ' + strTime + ' #' + j,
-                height: Math.max(50, Math.floor(Math.random() * 150)),
-                day: strTime
-              });
-            }
-          }
-        }
+    staticData.auctions.forEach(auction => {
+      const strTime = auction.startDate.split('T')[0];
+      if (!newItems[strTime]) {
+        newItems[strTime] = [];
       }
-      console.log("newItems length - ", newItems.length);
-      setItems(newItems);
-    }, 1000);
-  };
+      newItems[strTime].push({
+        name: auction.name,
+        height: 50,
+        day: strTime
+      });
+    });
+
+    setItems(newItems);
+  }, []);
 
   const renderDay = (day) => {
     if (day) {
@@ -49,7 +142,6 @@ export default function TabTwoScreen() {
     return (
       <TouchableOpacity
         testID={testIDs.agenda.ITEM}
-        // style={[styles.item, { height: reservation.height }]}
         style={[styles.item]}
         onPress={() => Alert.alert(reservation.name)}>
         <Text style={{ fontSize, color }}>{reservation.name}</Text>
@@ -69,11 +161,6 @@ export default function TabTwoScreen() {
     return r1.name !== r2.name;
   };
 
-  const timeToString = (time: number) => {
-    const date = new Date(time);
-    return date.toISOString().split('T')[0];
-  };
-
   const currentDate = new Date();
   const minDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1);
   const maxDate = new Date(currentDate.getFullYear() + 1, currentDate.getMonth(), 1);
@@ -82,7 +169,7 @@ export default function TabTwoScreen() {
     <Agenda
       testID={testIDs.agenda.CONTAINER}
       items={items}
-      loadItemsForMonth={loadItems}
+      loadItemsForMonth={() => {}}
       selected={currentDate.toISOString().split('T')[0]}
       renderItem={renderItem}
       renderEmptyDate={renderEmptyDate}
